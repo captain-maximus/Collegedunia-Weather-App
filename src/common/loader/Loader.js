d8ae734bb0eee1efb255ee3d942e95abf4083372
Suraj Yadav <79012753+captain-maximus@users.noreply.github.com>
2021-08-09 13:20:02 +0530

    Created Necessary Folders & Files

---
+++ src/common/loader/Loader.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const Loader = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  )
+}
+
+export default Loader
